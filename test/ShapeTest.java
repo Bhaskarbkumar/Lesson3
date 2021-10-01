b0d3ecb72b15e35d5cd89755ffbc18d462ae2082
bhaskarbkumar <bhaskarbkumar@gmail.com>
2021-09-30 20:45:11 -0500

    init

--- test/ShapeTest.java
+++ test/ShapeTest.java
@@ -3,12 +3,18 @@ package test;
 import java.util.ArrayList;
 import model.Shape;
 import java.awt.Color;
+
 import model.Circle;
 import model.Rectangle;
 ;
 public class ShapeTest {
 
     public static void main(String[] args){
+
+        float f1 = 1+0.01f+ccc+
+
+        Circle c1 = new Circle(1,1, Color.white,2);
+        assert c1.getX() == 1.1;
 
         ArrayList<Shape> shapes = new ArrayList<>();
         shapes.add(new Circle(0,0,Color.white,1));
